ca83b77bdc3d305702678dc1a49935632c17ebdb
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-05-16 22:03:36 +0530

    first commit

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,15 @@
+export const FeedbackModal = () => (
+  <>
+    <button
+      className="navbar-link"
+      data-tf-popup="Kjk8omIg"
+      data-tf-opacity="100"
+      data-tf-iframe-props="title=Filmingo"
+      data-tf-transitive-search-params
+      data-tf-medium="snippet"
+    >
+      Feedback
+    </button>
+    <script src="//embed.typeform.com/next/embed.js"></script>
+  </>
+);
