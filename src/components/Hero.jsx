71294e4cc89de0e7514078d967ce98dd1b77b78a
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-05-16 22:32:58 +0530

    fix: :sparkles: app.filmingo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,7 +54,9 @@ export const Hero = () => {
           <div className="flex flex-col gap-2 sm:flex-row mt-14 mb-24 sm:mb-40 justify-center">
             <div
               className="custom-button-colored w-64 sm:w-52 h-12 mr-0 sm:mr-4 lg:mr-6 mb-2 sm:mb-0"
-              onClick={() => setIsModalOpen(true)}
+              // onClick={() => setIsModalOpen(true)}
+              // navigate to https://app.filmingo.us/
+              onClick={() => window.open("https://app.filmingo.us/")}
             >
               Get Started
             </div>
