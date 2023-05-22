0c7c6b988603db7701e37badd1e460c5eb7fb35a
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-05-22 21:22:33 +0530

    modified:   src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,10 +29,11 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.05 }}
         >
           <div className="text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-8 md:px-20 lg:px-4">
-            <span>Watch Movies Together</span>
+            <span className="inline md:hidden">Watch Together</span>
+            <span className="hidden md:inline">Watch Movies Together</span>
           </div>
           <div className="mt-2 sm:mt-2 text-4xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-20 md:px-24 lg:px-24">
-            seamlessly.
+          seamlessly.
           </div>
         </motion.div>
         <motion.div
@@ -41,8 +42,8 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.1 }}
         >
           <div className="text-customGrayText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 ">
-            Experience the laughter filled synchronized movie playback, a
-            friendly collaboration tool!
+          Experience the laughter filled synchronized movie playback,
+          a friendly collaboration tool!
           </div>
         </motion.div>
         <motion.div
